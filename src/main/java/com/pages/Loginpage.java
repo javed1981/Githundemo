83ec6eb563a0b91a9c7491c74ba9b696c96a850e
javed1981 <jqkhan1891@gmail.com>
2022-08-07 20:50:27 +0400

    second commit

--- src/main/java/com/pages/Loginpage.java
+++ src/main/java/com/pages/Loginpage.java
@@ -2,10 +2,17 @@ package com.pages;
 
 public class Loginpage {
 	
+	
+	
 	public void loginPage()
 	{
 		
 		System.out.println("This is login page ");
+	}
+	
+	public void logOut() {
+		
+		System.out.println("Hello Logout");
 	}
 
 }
