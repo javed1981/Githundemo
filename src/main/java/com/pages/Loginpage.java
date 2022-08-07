fd29e1938b883fbe635543d3f1498f4134fa773e
javed1981 <jqkhan1891@gmail.com>
2022-08-07 19:42:44 +0400

    first commit

---
+++ src/main/java/com/pages/Loginpage.java
@@ -0,0 +1,11 @@
+package com.pages;
+
+public class Loginpage {
+	
+	public void loginPage()
+	{
+		
+		System.out.println("This is login page ");
+	}
+
+}
---
+++ src/test/java/com/testpages/Logintest.java
@@ -0,0 +1,11 @@
+package com.testpages;
+
+public class Logintest {
+	
+	public void loginTest()
+	{
+		
+		System.out.println("This is login pageTest ");
+	}
+
+}
